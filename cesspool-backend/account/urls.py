aae33f7c618198347b8f959b90b3f75bd389ce90
Henrich Micko <henrich.micko@spsjm.eu>
2022-07-17 12:13:43 +0200

    changed settings.SIMPLE_JWT['AUTH_HEADER_TYPES'] from 'JWT' to 'Bearer'

--- cesspool-backend/account/urls.py
+++ cesspool-backend/account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("logout/", views.BlackListTokenView.as_view(), name = "blacklist"),
     path('token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    path("hello/", views.TestView.as_view(), name = "hello")
 ]
--- cesspool-backend/account/views.py
+++ cesspool-backend/account/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -34,3 +34,10 @@ class BlackListTokenView(APIView):
         token.blacklist()
 
         return Response(status = status.HTTP_200_OK)
+
+
+class TestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"text": "hello world"}, status = status.HTTP_200_OK)
