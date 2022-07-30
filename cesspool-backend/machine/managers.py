a3991cf1fbf794b4ff234bc4c1accea5bf3fe9d7
Henrich Micko <henrich.micko@spsjm.eu>
2022-07-30 23:41:05 +0200

    added get_year/month/day_period method into machine.managers.RecordQuearySet to view records from last 24 hours...

--- cesspool-backend/machine/managers.py
+++ cesspool-backend/machine/managers.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from datetime import date, timedelta
 
 
 class RecordQuerySet(QuerySet):
@@ -8,3 +9,21 @@ class RecordQuerySet(QuerySet):
         for obj in self.all():
             output += obj.level
         return output/len(self.all())
+
+    def get_year_period(self):
+        today = date.today()
+        output = self.filter(date__range = [today, today - timedelta(years = 1)])
+
+        return output
+
+    def get_month_period(self):
+        today = date.today()
+        output = self.filter(date__range = [today, today - timedelta(months = 1)])
+
+        return output
+
+    def get_day_period(self):
+        today = date.today()
+        output = self.filter(date__range = [today, today - timedelta(hours = 24)])
+
+        return output
