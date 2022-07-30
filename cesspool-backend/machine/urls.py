e9bf720f621c6c97e03b7060f9800ae818ebc088
Henrich Micko <henrich.micko@spsjm.eu>
2022-07-30 16:56:55 +0200

    added machine records view

--- cesspool-backend/machine/urls.py
+++ cesspool-backend/machine/urls.py
@@ -3,5 +3,6 @@ from  . import views
 
 urlpatterns = [
     path("my/", views.my_machines_view, name = "machine_my_machine"),
-    path("<str:code>/", views.machine_detail_view, name = "machine_machine_detail")
+    path("<str:code>/", views.machine_detail_view, name = "machine_machine_detail"),
+    path("<str:code>/records/", views.records_view, name = "machine_records")
 ]
--- cesspool-backend/machine/views.py
+++ cesspool-backend/machine/views.py
@@ -28,3 +28,19 @@ class MyMachinesAPIView(APIView):
         return Response(machines_serializer.data, status = status.HTTP_200_OK)
 
 my_machines_view = MyMachinesAPIView.as_view()
+
+
+class RecordsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, code: str):
+        machine = request.user.machine_set.get(code = code)
+        if machine == None:
+            return Response({"detail": "This user doesn't own machine with this code."}, status = status.HTTP_404_NOT_FOUND)
+
+        records = machine.record_set.all()
+        records_serializer = serializers.RecordSerializer(instance = records, many = True)
+        
+        return Response(records_serializer.data, status = status.HTTP_200_OK)
+
+records_view = RecordsAPIView.as_view()
