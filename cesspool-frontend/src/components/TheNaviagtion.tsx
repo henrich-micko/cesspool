7cd6ca45799f494247cca432a9937878d559d503
Henrich Micko <henrich.micko@spsjm.eu>
2022-08-28 18:42:43 +0200

    fixed admin navlink not active with post params

--- cesspool-frontend/src/components/TheNaviagtion.tsx
+++ cesspool-frontend/src/components/TheNaviagtion.tsx
@@ -9,9 +9,13 @@ import AuthContext from "../context/AuthContext"
 import useIsMobile from "../hooks/useIsMobile"
 
 
-const TheNaviagtion: React.FC = () => {
+const TheNaviagtion: React.FC = (props) => {
     const { isLogged, user } = useContext(AuthContext)
     const isMobile = useIsMobile()
+
+    const isUrlActive = (url: string): boolean => {
+        return window.location.pathname.startsWith(url)
+    }
  
     return(
         <nav className={styles.navigation}>
@@ -24,7 +28,7 @@ const TheNaviagtion: React.FC = () => {
                 </> :
                 <>
                     <NavLink to="/machine" className={({ isActive }) => isActive ? styles.active : undefined}>Zariadenia</NavLink>
-                    {user.is_superuser && <NavLink to="/admin/machine" className={({ isActive }) => isActive ? styles.active : undefined}>Admin</NavLink>}
+                    {user.is_superuser && <NavLink to="/admin/machine" className={() => isUrlActive("/admin") ? styles.active : undefined}>Admin</NavLink>}
                     <NavLink to="/account" className={({ isActive }) => isActive ? styles.active : undefined}>Učet{!isMobile && ": " + user.email }</NavLink>
                 </>
             }
