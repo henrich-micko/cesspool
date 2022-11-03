882ac19cf6db3284fe1c8cc1c5575937bba1172d
Henrich Micko <henrich.micko@spsjm.eu>
2022-11-03 08:29:33 +0100

    switched to npm and added suport for absolute paths out of the box

---
+++ cesspool-frontend/craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    webpack: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@context': path.resolve(__dirname, 'src/context'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@types': path.resolve(__dirname, 'src/types'),
+            '@permissions': path.resolve(__dirname, 'src/permissions'),
+            '@views': path.resolve(__dirname, 'src/views'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+
+        },
+    },
+};
